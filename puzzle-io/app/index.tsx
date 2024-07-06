2d5e61e46ddc69bfc1e2ddeb3f5af56ee762f757
karelbohac <bohac@qualityregistry.org>
2024-07-07 00:56:24 +0200

    added limits toempty cell only

--- puzzle-io/app/index.tsx
+++ puzzle-io/app/index.tsx
@@ -94,7 +94,6 @@ export default function Index() {
     >
 
       <StatusBar hidden={true} />
-
     </GameEngine>
   );
 }
--- puzzle-io/system/touch/touches.ts
+++ puzzle-io/system/touch/touches.ts
@@ -1,19 +1,41 @@
 import { IEntity, EventItem } from "../gameEngine/DefaultRenderer";
 
 const MoveFinger = (entities: IEntity[], event: EventItem) => {
-  console.log(4, event, entities);
+    const mapToString = (map: {x: number, y: number}) => `${map.x}-${map.y}`;
+    const ocupiedCells = entities.map(entity => mapToString(entity.map));
     return (
       entities.map(entity => {
       if(entity.key === event.entity?.key) {
-        console.log(8, entity.key, event.entity?.key)
-        if(event.type === 'end') {
+        const newMap = {
+          x: Math.floor(event.touch.snapX / 20),
+          y: Math.floor(event.touch.snapY / 20),
+        }
+        if(event.type === 'start') {
           return {
             ...entity,
-            position: {
+            init: {
               ...entity.position,
               x: event.touch.snapX,
               y: event.touch.snapY,
             },
+            position: {
+              ...entity.position,
+              z: 99,            }
+          };
+        }
+        if(event.type === 'end') {
+          const isOccupied = ocupiedCells.filter(cell => cell === mapToString(newMap)).length > 0;
+          console.log(isOccupied, ocupiedCells, mapToString(newMap), {...entity.init});
+          return {
+            ...entity,
+            position: {
+              ...entity.position,
+              z: 1,
+              x: isOccupied ? entity.init.x : event.touch.snapX,
+              y: isOccupied ? entity.init.y : event.touch.snapY,
+            },
+            map: isOccupied ? entity.map : newMap,
+            key: Math.random().toString(36).substring(7),
           };
         }
         return {
