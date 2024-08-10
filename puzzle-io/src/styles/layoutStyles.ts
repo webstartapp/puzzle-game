5e0cb5985424d4b0d9013dcde62297a382553cd9
karelbohac <bohac@qualityregistry.org>
2024-08-10 21:47:18 +0200

    fixed css of container

--- puzzle-io/src/styles/layoutStyles.ts
+++ puzzle-io/src/styles/layoutStyles.ts
@@ -5,7 +5,6 @@ export const layoutStyles = StyleSheet.create({
     display: 'flex',
     width: '100%',
     height: '100%',
-    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
